98ebca49e243e05ff960483da1f42fd578eaa9d1
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-08 10:39:24 +0700

    PSEUDOCODE FOR THE SIMPLE CALCULATOR ONLY AND WITHOUT APPROPRIATE EXCEPTIONS TO CAPTURE ERRORS

---
+++ SimpleCalculator_maincode.py
@@ -0,0 +1,22 @@
+#Create a Simple App Calculator
+
+#Ask the user to choose one of the four math operations: Addition, Subtraction, Multiplication, and Division.
+#IF the user chose Addition:
+    #THEN ask the user to input two numbers. Can be float and integers.
+    #Perform the operation.
+    #Display the Result 
+#ELIF the user chose subtraction:
+    #THEN ask the user to input two numbers. Can be float and integers.
+    #Perform the operation.
+    #Display the Result 
+#ELIF the user chose multiplication:
+    #THEN ask the user to input two numbers. Can be float and integers.
+    #Perform the operation.
+    #Display the Result 
+#ELSE, IF the user chose multiplication:
+    #THEN ask the user to input two numbers. Can be float and integers.
+    #Perform the operation.
+    #Display the Result 
+#Ask if user wants another calculation
+#IF the answer in "no":
+    #THEN end the program
