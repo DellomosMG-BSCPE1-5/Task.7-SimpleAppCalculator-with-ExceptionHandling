883945ef74b5e554c38085a97485c4c87473de6f
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-09 22:49:38 +0700

    Exception Handling; VALUEERROR (scratch)

--- Scratch.py
+++ Scratch.py
@@ -10,40 +10,46 @@ def Calculator ():
     print("\t[4] Division")
 
     #Ask the user to choose one of the four math operations: Addition, Subtraction, Multiplication, and Division.
-    option_number = int(input("\nKindly enter the number that corresponds to the operation you want to perform: "))
+    try:
+        option_number = int(input("\nKindly enter the number that corresponds to the operation you want to perform: "))
 
-    #If the user's choice is one of the ones in the menu:
-    if option_number <= 4:
-        #Ask the user to input two numbers. Can be float and integers.
-        number_1 = float(input("Enter first number: "))
-        number_2 = float(input("Enter second number: "))
+        #If the user's choice is one of the ones in the menu:
+        if option_number <= 4:
+            #Ask the user to input two numbers. Can be float and integers.
+            number_1 = float(input("Enter first number: "))
+            number_2 = float(input("Enter second number: "))
+            
+            #IF the user chose Addition:
+            if option_number == 1:
+                #Display the Result 
+                print(number_1 + number_2) 
+
+            #ELIF the user chose subtraction:
+            elif option_number == 2:
+                #Display the Result
+                print(number_1 - number_2) 
+
+            #ELIF the user chose multiplication:
+            elif option_number == 3:
+                #Display the Result
+                print(number_1 * number_2) 
+
+            #ELSE, if the user chose Division:
+            else:
+                #Perform the operation.
+                quotient = number_1 / number_2
+                #Display the Result 
+                print(format(quotient, ".3f"))
         
-        #IF the user chose Addition:
-        if option_number == 1:
-            #Display the Result 
-            print(number_1 + number_2) 
-
-        #ELIF the user chose subtraction:
-        elif option_number == 2:
-            #Display the Result
-            print(number_1 - number_2) 
-
-        #ELIF the user chose multiplication:
-        elif option_number == 3:
-            #Display the Result
-            print(number_1 * number_2) 
-
-        #ELSE, if the user chose Division:
+        #If the user's choice is one of the ones in the menu:
         else:
-            #Perform the operation.
-            quotient = number_1 / number_2
-            #Display the Result 
-            print(format(quotient, ".3f"))
-    
-    #If the user's choice is one of the ones in the menu:
-    else:
-        print("\nYou have not typed a valid number. Please enter a number from the menu (1-4).\n")
+            print("\nYou have not typed a valid number. Please enter a number from the menu (1-4).\n")
+            Calculator()
+
+    except ValueError:
+        print("Invalid Input. Please type your number as numeric characters")
         Calculator()
+  
 
 #Ask if user wants another calculation
 #IF the answer in "no":
