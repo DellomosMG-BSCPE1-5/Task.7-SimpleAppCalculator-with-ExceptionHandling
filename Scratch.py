d57ea2dae12e5464711d7f2cb562697483346d19
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-10 11:29:49 +0700

    FINAL CODE

--- Scratch.py
+++ Scratch.py
@@ -1,21 +1,21 @@
 #Create a Simple App Calculator
 #Program title
-from art import *
-from colorama import Fore, Back, Style
-program_title = text2art("CALCULATE  IT!", font='tarty1', chr_ignore=True)
-print(Fore.CYAN + program_title)
-
-#Header
-from pyboxen import boxen
-print(
-        boxen(
-            "  Welcome to Calculate it! A calculator for every need, accuracy at your fingertips.  ",
-            color="cyan",
-        )
-    )
-
-#Make a menu for the options (Addition, Subtraction, Multiplication, Division)
 while True:
+    from art import *
+    from colorama import Fore, Back, Style
+    program_title = text2art("CALCULATE  IT!", font='tarty1', chr_ignore=True)
+    print(Fore.CYAN + program_title)
+
+    #Header
+    from pyboxen import boxen
+    print(
+            boxen(
+                "  Welcome to Calculate it! A calculator for every need, accuracy at your fingertips.  ",
+                color="cyan",
+            )
+        )
+
+    #Make a menu for the options (Addition, Subtraction, Multiplication, Division)
     def menu ():
         options_label = text2art("OPTION: ", font='lilia', chr_ignore=True)
         print(Fore.CYAN + options_label)
@@ -43,36 +43,76 @@ while True:
                 #If the user's choice is one of the ones in the menu:
                 if 0 < option_number <= 4:
                     #Then ask the user to input two numbers. Can be float and integers.
-                    number_1 = float(input("Enter first number: "))
-                    number_2 = float(input("Enter second number: "))
+                    number_1 = float(input("\n\tEnter first number: "))
+                    number_2 = float(input("\tEnter second number: "))
                     
+                    #Loading...
+                    from rich.progress import track
+                    from time import sleep
+                    print('\n')
+                    def process_data():
+                        sleep(0.01)
+                    for _ in track(range(100), description='[blue]Processing data'):
+                        process_data()
+
                     #IF the user chose type 1 (Addition):
                     if option_number == 1:
                         #Perform operation and display the Result 
-                        print(number_1 + number_2) 
+                        from rich.console import Console
+                        print("\n")
+                        print(Fore.BLUE + "=" * 105)
+                        console = Console()
+                        console.print("\n", "[cyan]The sum is[cyan]: ", number_1 + number_2)
+
                     #ELIF the user chose subtraction:
                     elif option_number == 2:
                         #Perform operation and display the Result 
-                        print(number_1 - number_2) 
+                        from rich.console import Console
+                        print("\n")
+                        print(Fore.BLUE + "=" * 105)
+                        console = Console()
+                        console.print("\n", "[cyan]The difference is[cyan]: ", number_1 - number_2)
+
                     #ELIF the user chose multiplication:
                     elif option_number == 3:
                         #Perform operation and display the Result 
-                        print(number_1 * number_2) 
+                        from rich.console import Console
+                        print("\n")
+                        print(Fore.BLUE + "=" * 105)
+                        console = Console()
+                        console.print("\n", "[cyan]The product is[cyan]: ", number_1 * number_2)
+
                     #ELSE, if the user chose Division:
                     else: 
                         try:
                             #Perform the operation.
                             quotient = number_1 / number_2
                             #Display teh result
-                            print(format(quotient, ".3f"))
+                            from rich.console import Console
+                            print("\n")
+                            print(Fore.BLUE + "=" * 105)
+                            console = Console()
+                            console.print("\n", "[cyan]The quotient is[cyan]: ", quotient)
+
                         #except there's a ZeroDivisionError
                         except ZeroDivisionError:
-                            print("Zero Division Error: Second number cannot be 0. Please enter again. ")
+                            print("\n")
+                            print(
+                                    boxen(
+                                        "  !!! Zero Division Error: Second number cannot be 0. Please enter again.  ",
+                                        color="cyan",
+                                    )
+                                )                            
                             calculate()
                                             
                 #If the user's choice is not one from the menu:
                 else:
-                    print("\nYou have not typed a valid number. Please enter a number that is from the menu (1-4).\n")
+                    print(
+                            boxen(
+                                "You have not typed a valid number. Please enter a number that is from the menu (1-4).",
+                                color="cyan",
+                            )
+                        )
                     menu()
             
             calculate()
@@ -83,7 +123,7 @@ while True:
     if check.upper() == "Y":  # go back to the top
         continue
     #IF the answer in "no":
-    print("Thank you for using this program!")
+    print(Fore.CYAN + "\nThank you for using this program! Have a Nice Day!")
     #THEN end the program
     break
 
