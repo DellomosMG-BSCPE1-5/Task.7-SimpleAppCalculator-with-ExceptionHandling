aec34872aba43e0c22626a9fa0118798f12b3606
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-05-10 21:48:50 +0700

    REVISED CODE: ValueError INSERTED

--- REVISED CODE.py
+++ REVISED CODE.py
@@ -9,8 +9,13 @@ def calculator():
     print("\t[3] Multiplication")
     print("\t[4] Division")
 
+    try:
     #Ask the user to choose one of the four math operations: Addition, Subtraction, Multiplication, and Division.
-    option_number = float(input("\nKindly enter the number that corresponds to the operation you want to perform: "))
+        option_number = float(input("\nKindly enter the number that corresponds to the operation you want to perform: "))
+    #If the user enter a character other than numeric   
+    except ValueError as ERROR:
+        print("Invalid Input. Please type your number as numeric character.")
+        calculator() 
 
     #if the user's choice is one of the ones in the menu:
     if 0 < option_number <= 4:
@@ -34,14 +39,14 @@ def calculator():
             print(first_number * second_number)
 
         #if the user chose Division:
-        elif option_number == 4:
+        else:
             #Perform operation and display the result 
             print(first_number / second_number)
 
 #If the user's choice is not one from the menu:
     else:
     #then inform the user that his/her input is invalid.
-        print("Invalid")
+        print("You have not typed a valid number. Please enter a number that is from the menu (1-4).")
 
 #Ask if user wants another calculation
 #IF the answer in "no":
